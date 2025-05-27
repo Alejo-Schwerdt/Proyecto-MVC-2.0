0f9c457ba3d79d2e23c032e33e2178f2fca97489
Alejo Schwerdt <alejoschwerdt2002@gmail.com>
2025-05-27 13:50:32 -0300

    Add files via upload

---
+++ Prueba-MCV/VentasProducto.Designer.cs
@@ -0,0 +1,152 @@
+﻿using System.Windows.Forms;
+
+namespace Prueba_MCV
+{
+    partial class VentasProducto
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ComboBox cmbProducto;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnRegistrarVenta;
+        private System.Windows.Forms.DataGridView dgvDetalleVenta;
+        private System.Windows.Forms.Panel panelControles;
+        private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.Button btnReiniciar;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.panelControles = new System.Windows.Forms.Panel();
+            this.cmbProducto = new System.Windows.Forms.ComboBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnRegistrarVenta = new System.Windows.Forms.Button();
+            this.btnReiniciar = new System.Windows.Forms.Button();
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.dgvDetalleVenta = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panelControles.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetalleVenta)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelControles
+            // 
+            this.panelControles.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panelControles.Controls.Add(this.label2);
+            this.panelControles.Controls.Add(this.label1);
+            this.panelControles.Controls.Add(this.cmbProducto);
+            this.panelControles.Controls.Add(this.txtCantidad);
+            this.panelControles.Controls.Add(this.btnAgregar);
+            this.panelControles.Controls.Add(this.btnRegistrarVenta);
+            this.panelControles.Controls.Add(this.btnReiniciar);
+            this.panelControles.Controls.Add(this.btnVolver);
+            this.panelControles.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelControles.Location = new System.Drawing.Point(0, 0);
+            this.panelControles.Name = "panelControles";
+            this.panelControles.Size = new System.Drawing.Size(800, 108);
+            this.panelControles.TabIndex = 1;
+            // 
+            // cmbProducto
+            // 
+            this.cmbProducto.Location = new System.Drawing.Point(18, 54);
+            this.cmbProducto.Name = "cmbProducto";
+            this.cmbProducto.Size = new System.Drawing.Size(180, 21);
+            this.cmbProducto.TabIndex = 0;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(204, 55);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(69, 20);
+            this.txtCantidad.TabIndex = 1;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(283, 23);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(90, 25);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnRegistrarVenta
+            // 
+            this.btnRegistrarVenta.Location = new System.Drawing.Point(475, 23);
+            this.btnRegistrarVenta.Name = "btnRegistrarVenta";
+            this.btnRegistrarVenta.Size = new System.Drawing.Size(120, 25);
+            this.btnRegistrarVenta.TabIndex = 3;
+            this.btnRegistrarVenta.Text = "Finalizar Pedido";
+            this.btnRegistrarVenta.Click += new System.EventHandler(this.btnRegistrarVenta_Click);
+            // 
+            // btnReiniciar
+            // 
+            this.btnReiniciar.Location = new System.Drawing.Point(379, 23);
+            this.btnReiniciar.Name = "btnReiniciar";
+            this.btnReiniciar.Size = new System.Drawing.Size(90, 25);
+            this.btnReiniciar.TabIndex = 4;
+            this.btnReiniciar.Text = "Reiniciar";
+            this.btnReiniciar.Click += new System.EventHandler(this.btnReiniciar_Click);
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.Location = new System.Drawing.Point(665, 23);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(132, 25);
+            this.btnVolver.TabIndex = 5;
+            this.btnVolver.Text = "Volver al menu principal";
+            this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
+            // 
+            // dgvDetalleVenta
+            // 
+            this.dgvDetalleVenta.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvDetalleVenta.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dgvDetalleVenta.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvDetalleVenta.Location = new System.Drawing.Point(0, 108);
+            this.dgvDetalleVenta.Name = "dgvDetalleVenta";
+            this.dgvDetalleVenta.Size = new System.Drawing.Size(800, 342);
+            this.dgvDetalleVenta.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Selecciona un producto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(204, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Cantidad";
+            // 
+            // VentasProducto
+            // 
+            this.Controls.Add(this.dgvDetalleVenta);
+            this.Controls.Add(this.panelControles);
+            this.Name = "VentasProducto";
+            this.Size = new System.Drawing.Size(800, 450);
+            this.panelControles.ResumeLayout(false);
+            this.panelControles.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetalleVenta)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private Label label2;
+        private Label label1;
+    }
+}
---
+++ Prueba-MCV/VentasProducto.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Model;
+using Controler;
+
+namespace Prueba_MCV
+{
+    public partial class VentasProducto: UserControl
+    {
+        private ProductoController productoController = new ProductoController();
+        private List<Producto> listaProductos = new List<Producto>();
+        private List<DetalleVenta> detallesVenta = new List<DetalleVenta>();
+        public event Action VolverAListar;
+
+        public VentasProducto()
+        {
+            InitializeComponent();
+            dgvDetalleVenta.AutoGenerateColumns = true;
+            CargarProductos();
+        }
+
+        private void CargarProductos()
+        {
+            listaProductos = productoController.ObtenerTodosLosProductos();
+
+            if (listaProductos == null || listaProductos.Count == 0)
+            {
+                MessageBox.Show("No hay productos disponibles.");
+                return;
+            }
+
+            cmbProducto.DataSource = listaProductos;
+            cmbProducto.DisplayMember = "nombreproducto"; 
+            cmbProducto.ValueMember = "idproducto";
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (cmbProducto.SelectedItem == null || !int.TryParse(txtCantidad.Text, out int cantidad) || cantidad <= 0)
+            {
+                MessageBox.Show("Selecciona un producto y una cantidad válida.");
+                return;
+            }
+
+            Producto producto = (Producto)cmbProducto.SelectedItem;
+
+            if (cantidad > producto.stockproducto)
+            {
+                MessageBox.Show($"La cantidad solicitada ({cantidad}) supera el stock disponible ({producto.stockproducto}).");
+                return;
+            }
+
+            DetalleVenta detalle = new DetalleVenta
+            {
+                IdProducto = producto.idproducto,
+                Cantidad = cantidad,
+                PrecioUnitario = producto.valorproducto,
+                Producto = producto
+            };
+
+            detallesVenta.Add(detalle);
+            ActualizarGrid();
+        }
+
+        private void ActualizarGrid()
+        {
+            dgvDetalleVenta.DataSource = null;
+            dgvDetalleVenta.DataSource = detallesVenta.Select(d => new
+            {
+                Producto = d.Producto.nombreproducto,
+                Cantidad = d.Cantidad,
+                PrecioUnitario = d.PrecioUnitario,
+                Subtotal = d.Cantidad * d.PrecioUnitario
+            }).ToList();
+        }
+
+        private void btnRegistrarVenta_Click(object sender, EventArgs e)
+        {
+            if (detallesVenta.Count == 0)
+            {
+                MessageBox.Show("Agrega al menos un producto a la venta.");
+                return;
+            }
+
+            Venta venta = new Venta
+            {
+                FechaVenta = DateTime.Now,
+                Detalles = detallesVenta
+            };
+
+            venta.TotalVenta = detallesVenta.Sum(d => d.Cantidad * d.PrecioUnitario);
+
+            VentaController controlador = new VentaController();
+            bool resultado = controlador.RegistrarVenta(venta);
+
+            if (resultado)
+            {
+                MessageBox.Show("Venta registrada con éxito.");
+                detallesVenta.Clear();
+                ActualizarGrid();
+            }
+            else
+            {
+                MessageBox.Show("Venta registrada con éxito.");
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void btnReiniciar_Click(object sender, EventArgs e)
+        {
+            detallesVenta.Clear();
+            ActualizarGrid();
+        }
+
+        private void btnVolver_Click(object sender, EventArgs e)
+        {
+            // Buscar el formulario contenedor (Form1)
+            Form1 form = this.FindForm() as Form1;
+            if (form != null)
+            {
+                // Llamar al método público de Form1 para volver al listado
+                form.MostrarFormularioListar();
+            }
+        }
+    }
+}
